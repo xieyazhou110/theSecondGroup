82662a6415e71daf5cc3f6c5a5fa7873d6eca608
wumingshuai <15839606960@163.com>
2016-10-23 16:43:06 +0800

    Initial Commit

---
+++ theSecondGroup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  theSecondGroup
+//
+//  Created by 吴明帅 on 16/10/23.
+//  Copyright © 2016年 TuHu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
