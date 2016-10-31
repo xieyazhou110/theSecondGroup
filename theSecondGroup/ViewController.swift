727c492508f274b433c52793963dfe547c140125
xyz <xieyazhou110@163.com>
2016-10-31 14:41:41 +0800

    测试改动

--- theSecondGroup/ViewController.swift
+++ theSecondGroup/ViewController.swift
@@ -17,8 +17,20 @@ class ViewController: UIViewController,UIScrollViewDelegate {
     fileprivate var bigImageArray = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        smallImageArray = ["http://p1.bqimg.com/4851/1cd9adb87fe173ab.png","http://i1.piimg.com/4851/60bc2453426cb1de.png","http://i1.piimg.com/4851/e163fb922bb1095f.png","http://i1.piimg.com/4851/ac117321df20c609.png","http://p1.bqimg.com/4851/12b153801d96e921.png","http://i1.piimg.com/4851/dfa3305821c72de6.png"]
-        bigImageArray = ["http://p1.bqimg.com/4851/40b42fcf1d12a0f6.png","http://i1.piimg.com/4851/ae8d058ad9dd248a.png","http://i1.piimg.com/4851/7a6d02e5bf2b8099.png","http://i1.piimg.com/4851/34bf31cb38c83d2c.png","http://p1.bqimg.com/4851/a9696a035f6774f3.png","http://i1.piimg.com/4851/9b9d8970c9c8e4a2.png"]
+        smallImageArray = ["http://p1.bqimg.com/4851/1cd9adb87fe173ab.png",
+                           "http://i1.piimg.com/4851/60bc2453426cb1de.png",
+                           "http://i1.piimg.com/4851/e163fb922bb1095f.png",
+                           "http://i1.piimg.com/4851/ac117321df20c609.png",
+                           "http://p1.bqimg.com/4851/12b153801d96e921.png",
+                           "http://i1.piimg.com/4851/dfa3305821c72de6.png"]
+        
+        bigImageArray = ["http://p1.bqimg.com/4851/40b42fcf1d12a0f6.png",
+                         "http://i1.piimg.com/4851/ae8d058ad9dd248a.png",
+                         "http://i1.piimg.com/4851/7a6d02e5bf2b8099.png",
+                         "http://i1.piimg.com/4851/34bf31cb38c83d2c.png",
+                         "http://p1.bqimg.com/4851/a9696a035f6774f3.png",
+                         "http://i1.piimg.com/4851/9b9d8970c9c8e4a2.png"]
+        
         let backView = UIView(frame: CGRect(x: 0,y: 20,width: ScreenWidth,height: ScreenWidth/2))
         let scrollView = self.createScrollView(frame:CGRect(x: 0,y: 20,width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.width/2), imageCount: smallImageArray.count)
         backView.addSubview(scrollView)
